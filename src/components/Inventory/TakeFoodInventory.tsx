69bdb939d7b17346556c63eb925298a21457bffa
Charles Leighton <chleighton1@gmail.com>
2024-07-04 19:23:04 -0400

    Add icons for taking inventory on mobile

--- src/components/Inventory/TakeFoodInventory.tsx
+++ src/components/Inventory/TakeFoodInventory.tsx
@@ -8,13 +8,41 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function FoodInventory(props: any) {
   const { food, setOpen, fetchArchive } = props;
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   const [foodItems, setFoodItems] = useState(
     food.map((item: any) => ({ ...item, stock: 0 }))
   );
+
+  const handleDecreaseStock = (id) => {
+    const updatedItems = foodItems.map((item) => {
+      if (item._id === id && item.stock > 0) {
+        return { ...item, stock: item.stock - 1 };
+      }
+      return item;
+    });
+    setFoodItems(updatedItems);
+  };
+
+  const handleIncreaseStock = (id) => {
+    const updatedItems = foodItems.map((item) => {
+      if (item._id === id) {
+        return { ...item, stock: item.stock + 1 };
+      }
+      return item;
+    });
+    setFoodItems(updatedItems);
+  };
 
   const handleStockChange = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -56,53 +84,109 @@ export default function FoodInventory(props: any) {
 
   return (
     <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 600 }}>Food Item</TableCell>
-              <TableCell sx={{ fontWeight: 600 }} align="right">
-                Unit
-              </TableCell>
-              <TableCell sx={{ fontWeight: 600 }} align="right">
-                Qty/Unit
-              </TableCell>
-              <TableCell sx={{ fontWeight: 600 }} align="right">
-                Par
-              </TableCell>
-              <TableCell sx={{ fontWeight: 600 }} align="right">
-                Stock
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {food.map((item: any) => (
-              <TableRow
-                key={item._id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {item.name}
+      {mobile ? (
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell
+                  sx={{ fontWeight: 600, width: "33%" }}
+                  align="center"
+                >
+                  Food Item
                 </TableCell>
-                <TableCell align="right">{item.unit}</TableCell>
-                <TableCell align="right">{item.qtyPerUnit}</TableCell>
-                <TableCell align="right">{item.par}</TableCell>
-                <TableCell align="right">
-                  <TextField
-                    id="outlined-number"
-                    type="number"
-                    size="small"
-                    style={{ maxWidth: "80px" }}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      handleStockChange(e, item._id)
-                    }
-                  />
+                <TableCell
+                  sx={{ fontWeight: 600, width: "33%" }}
+                  align="center"
+                >
+                  Par
+                </TableCell>
+                <TableCell
+                  sx={{ fontWeight: 600, width: "33%" }}
+                  align="center"
+                >
+                  Stock
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {foodItems.map((item: any) => (
+                <TableRow
+                  key={item._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row" align="center">
+                    {item.name}
+                  </TableCell>
+                  <TableCell align="center">{item.par}</TableCell>
+                  <TableCell align="center">
+                    <Stack direction="row" spacing={1}>
+                      <RemoveCircleIcon
+                        onClick={() => handleDecreaseStock(item._id)}
+                        style={{ color: "orange" }}
+                      />
+                      <span>{item.stock}</span>
+
+                      <AddCircleIcon
+                        onClick={() => handleIncreaseStock(item._id)}
+                        color="primary"
+                      />
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: 600 }}>Food Item</TableCell>
+                <TableCell sx={{ fontWeight: 600 }} align="right">
+                  Unit
+                </TableCell>
+                <TableCell sx={{ fontWeight: 600 }} align="right">
+                  Qty/Unit
+                </TableCell>
+                <TableCell sx={{ fontWeight: 600 }} align="right">
+                  Par
+                </TableCell>
+                <TableCell sx={{ fontWeight: 600 }} align="right">
+                  Stock
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {food.map((item: any) => (
+                <TableRow
+                  key={item._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {item.name}
+                  </TableCell>
+                  <TableCell align="right">{item.unit}</TableCell>
+                  <TableCell align="right">{item.qtyPerUnit}</TableCell>
+                  <TableCell align="right">{item.par}</TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="outlined-number"
+                      type="number"
+                      size="small"
+                      style={{ maxWidth: "80px" }}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        handleStockChange(e, item._id)
+                      }
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
       <Button
         variant="contained"
         color="primary"
